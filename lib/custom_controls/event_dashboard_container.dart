e6ee30ce2d9d5a6014e3a92e309f4c12d91ed75e
Siva-Rajan-R <siva967763@gmail.com>
2025-05-15 13:35:39 +0530

    updated32 event add and update route fronend

---
+++ lib/custom_controls/event_dashboard_container.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+
+
+class EventDashboardContainer extends StatelessWidget {
+  final int eventCount;
+  final int totCount;
+  final String eventStatus;
+  final int eventAmnt;
+  final Color themeColor;
+
+  const EventDashboardContainer({
+    super.key,
+    required this.eventCount,
+    required this.totCount,
+    required this.eventStatus,
+    required this.eventAmnt,
+    required this.themeColor,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(6)),
+        boxShadow: [
+          BoxShadow(
+            color: themeColor,
+            blurRadius: 2,
+            spreadRadius: 2,
+            blurStyle: BlurStyle.outer,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          CircularPercentIndicator(
+            radius: 60.0,
+            lineWidth: 10.0,
+            percent: eventCount / totCount,
+            center: Text(
+              '$eventCount / $totCount',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            backgroundColor: Colors.grey.shade200,
+            circularStrokeCap: CircularStrokeCap.round,
+            progressColor: themeColor,
+          ),
+          SizedBox(width: 30),
+          Text(
+            eventStatus.toUpperCase(),
+            style: TextStyle(color: themeColor, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(width: 30),
+          Text("₹ $eventAmnt", style: TextStyle(fontWeight: FontWeight.w600)),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/event_dashboard.dart
+++ lib/pages/event_dashboard.dart
@@ -1,93 +1,169 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:sampleflutter/utils/network_request.dart';
 import 'package:sampleflutter/custom_controls/custom_appbar.dart';
-import 'package:sampleflutter/custom_controls/features_container.dart';
+import 'package:sampleflutter/custom_controls/event_dashboard_container.dart';
 
-List<Widget> rowBuilder(List<Map<String,dynamic>> items){
+List<Widget> rowBuilder(List items) {
   print("items ${items.length}");
-  List<Widget> rows=[];
-  List<Widget> temp=[];
+  List<Widget> rows = [];
+  List<Widget> temp = [];
 
-  int count=0;
+  int count = 0;
   // FeaturesContainer(svgLink: items[i]["svg"], label: items[i]["label"], shadowColor: items[i]["sc"], containerColor: items[i]["cc"])
-  for(int i=0 ; i<items.length ; i++){
-      print(i);
-      temp.add(FeaturesContainer(svgLink: items[i]["svg"], label: items[i]["label"], shadowColor: items[i]["sc"], containerColor: items[i]["cc"],route: items[i]["route"],));
-      count++;
-      if(count==3){
-        rows.add(Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: List.from(temp),));
-        rows.add(SizedBox(height: 10,));
-        temp.clear();
-        count=0;
-      }
+  for (int i = 0; i < items.length; i++) {
+    print(i);
+    temp.add(
+      EventDashboardContainer(
+        eventCount: items[i]['count'],
+        totCount: 10,
+        eventStatus: items[i]['status'],
+        eventAmnt: items[i]['tot_amount'],
+        themeColor:
+            items[i]['status'] == 'pending'
+                ? Colors.orange
+                : items[i]['status'] == 'canceled'
+                ? Colors.red
+                : Colors.green,
+      ),
+    );
+    count++;
+    if (count == 2) {
+      rows.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.from(temp),
+        ),
+      );
+      rows.add(SizedBox(height: 10));
+      temp.clear();
+      count = 0;
     }
-  if(temp.isNotEmpty){
-    rows.add(Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: List.from(temp),));
+  }
+  if (temp.isNotEmpty) {
+    rows.add(
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List.from(temp),
+      ),
+    );
     temp.clear();
   }
   print("rows length $rows");
   return rows;
+}
+
+class EventDashboardPage extends StatefulWidget {
+  const EventDashboardPage({super.key});
+  @override
+  State<EventDashboardPage> createState() => _EventDashboardPageState();
+}
+
+class _EventDashboardPageState extends State<EventDashboardPage> {
+  DateTime? fromDate;
+  DateTime? toDate;
+  List eventsDashboard=[];
+  bool isLoading = false;
+  
+    Future getEventDashboard(BuildContext context)async {
+     setState(() {
+       isLoading=true;
+      });
+    final res=await NetworkService.sendRequest(path: '/event/dashboard',context: context,body: {'from_date':fromDate,'to_date':toDate});
+    print("res $res");
+    setState(() {
+       isLoading=false;
+      });
+    if (res!=null){
+       eventsDashboard=List<Map>.from(res['events_dashboard']);
+    }
     
   }
-class EventDashboardPage extends StatelessWidget {
-  final List<Map<String, dynamic>> statusData = [
-    {'count': 5, 'tot_amount': 5000, 'status': 'pending'},
-    {'count': 3, 'tot_amount': 3000, 'status': 'completed'},
-    {'count': 2, 'tot_amount': 2000, 'status': 'canceled'},
-  ];
+
+  @override
+  void initState() {
+    super.initState();
+    getEventDashboard(context);
+  }
+
+  Future<void> pickFromDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.parse("2025-01-01"),
+      lastDate: DateTime(DateTime.now().year, 12, 31),
+    );
+    if (picked != null) {
+      setState(() {
+        fromDate = picked;
+      });
+    }
+  }
+
+  Future<void> pickToDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.parse("2025-01-01"),
+      lastDate: DateTime(DateTime.now().year, 12, 31),
+    );
+    if (picked != null) {
+      setState(() {
+        toDate = picked;
+      });
+
+      await getEventDashboard(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: KovilAppBar(),
-      body: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Container(
-          constraints: BoxConstraints(minHeight: 220), // ✅ Increased height
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularPercentIndicator(
-                radius: 60.0,
-                lineWidth: 10.0,
-                percent: 0.5,
-                center: Text(
-                  '688',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: isLoading? Center(child: CircularProgressIndicator(color: Colors.orange,),) 
+      : Column(
+        children: [
+          SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  foregroundColor: Colors.white,
                 ),
-
-                backgroundColor: Colors.grey.shade200,
-                circularStrokeCap: CircularStrokeCap.round,
+                onPressed: () => pickFromDate(context),
+                child: Text(
+                  fromDate == null
+                      ? "Pick Start Date"
+                      : "${fromDate!.toLocal()}".split(' ')[0],
+                ),
               ),
-              SizedBox(height: 12),
               Text(
-                "pending",
+                "To",
                 style: TextStyle(
-                  fontWeight: FontWeight.w600,
-
+                  fontSize: 20,
+                  color: Colors.orange,
+                  fontWeight: FontWeight.w600
                 ),
               ),
-              SizedBox(height: 6),
-              Text(
-                '₹ 5777',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black87,
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () => pickToDate(context),
+                child: Text(
+                  toDate == null
+                      ? "Pick End Date"
+                      : "${toDate!.toLocal()}".split(' ')[0],
                 ),
               ),
-            ],
-          ),
-        ),
-      )
-  
+              ],
+            ),
+              ...rowBuilder(eventsDashboard)
+            
+        ]
+      ),
     );
   }
 }
